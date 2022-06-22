3df91930e16277d13158abf9dda59713df38ddec
Cole Rossman <colerossman99@gmail.com>
2022-06-21 17:37:35 -0700

    insert function created for user in model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,22 +12,22 @@ const mockUser = {
   password: '12345',
 };
 
-const registerAndLogin = async (userProps = {}) => {
-  const password = userProps.password ?? mockUser.password;
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
 
-  // Create an "agent" that gives us the ability
-  // to store cookies between requests in a test
-  const agent = request.agent(app);
+//   // Create an "agent" that gives us the ability
+//   // to store cookies between requests in a test
+//   const agent = request.agent(app);
 
 
-  // create a user to sign in with
-  const user = await UserService.create({ ...mockUser, ...userProps });
+//   // create a user to sign in with
+//   const user = await UserService.create({ ...mockUser, ...userProps });
 
-  // ... then sign in 
-  const { email } = user;
-  await agent.post('/api/v1/users/sessions').send({ email, password });
-  return [agent, user];
-};
+//   // ... then sign in 
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
 
 describe('secrets routes', () => {
   beforeEach(() => {
@@ -49,5 +49,5 @@ describe('secrets routes', () => {
       email,
     });
   });
-  
+
 });
